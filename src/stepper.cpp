b9477afb18d0ce8180bdb046069b182ef99cd3c1
DefinitlyNotAHackerTrustMeIAmADolphin <metalhead.baschi@live.de>
2020-02-12 14:08:03 +0000

    zwischenspeichern

--- src/stepper.cpp
+++ src/stepper.cpp
@@ -2,12 +2,16 @@
 
 #include <stdio.h>
 
+#include <cmath>
+
 #include <iostream>
 #include <fstream>
 
-#define steps_per_mm 10
+#define steps_per_mm 40
 
-using namespace std;
+#define width 840
+
+//using namespace std;
 
 // Sequenz in der die Spulen des Schrittmotors angeschaltet werden müssen
 const bool sequence[][4] = {
@@ -31,7 +35,7 @@ struct Coord
 class stepper_motor {
 	char pins[4];
 	int current_step = 0;
-	ofstream pos;
+	std::ofstream output;
 	public:
 		stepper_motor (char, char, char, char);
 		void setup();
@@ -39,6 +43,8 @@ class stepper_motor {
 		int get_current_step();
 		void write_current_step(int);
 		void write_mm(double);
+		void save_cable_length();
+		void step_to(int, int);
 };
 
 // initialisierung mit Anschluss-Pins
@@ -54,15 +60,15 @@ void stepper_motor::setup() {
 	for(int i = 0; i < 4; i++) {
 		pinMode(pins[i], OUTPUT);
 	}
-	ifstream configfile;
-	configfile.open (to_string(pins[0]) + ".txt");
-	double mm << configfile;
-	current_step = mm * steps_per_mm;
-	configfile.close;
+	std::ifstream configfile;
+	configfile.open (std::to_string(pins[0]) + ".txt");
+	configfile >> current_step;
+	configfile.close();
 }
 
 // Mach schritte
-void stepper_motor::step(int steps, int speed = 100) {
+void stepper_motor::step(int steps, int speed = 10) {
+	std::cout << "gehe von " << current_step;
 	if (steps > 0) {
 		for (int s = steps + current_step; s > current_step; s--) {
 			for(int p = 0; p < 4; p++) {
@@ -80,6 +86,24 @@ void stepper_motor::step(int steps, int speed = 100) {
 		}
 	}
 	current_step += steps;
+	std::cout << " zu " << current_step << " steps\n";
+}
+
+void stepper_motor::step_to(int step, int speed = 10){
+	while (step < current_step){
+		for(int p = 0; p < 4; p++) {
+				digitalWrite(pins[p], sequence[current_step % 8][p]);
+			}
+		current_step--;
+		delay(speed);
+	}
+	while (step > current_step){
+		for(int p = 0; p < 4; p++) {
+				digitalWrite(pins[p], sequence[current_step % 8][p]);
+			}
+		current_step++;
+		delay(speed);
+	}
 }
 
 int stepper_motor::get_current_step(){
@@ -91,20 +115,59 @@ void stepper_motor::write_current_step(int lol){
 }
 
 void stepper_motor::write_mm(double mm) {
-	current_step = mm;
+	current_step = mm * steps_per_mm;
+	std::string filename = std::to_string(pins[0]) + ".txt";
+	output.open (filename.c_str());
+	output << current_step;
+	output.close();
+	std::cout << filename << '\n';
 }
 
-stepper_motor left (19, 16, 26, 20);
-stepper_motor right (17, 18, 22, 23);
+void stepper_motor::save_cable_length(){
+	std::string filename = std::to_string(pins[0]) + ".txt";
+	output.open (filename.c_str());
+	output << current_step;
+	output.close();
+	std::cout << "Kabellänge gespeichert" << filename << '\n';
+}
 
-std::ofstream left_store ("left.txt");
+stepper_motor right (19, 16, 26, 20);
+stepper_motor left (17, 18, 22, 23);
+
+int mm2steps(double mm){
+	return mm * steps_per_mm;
+}
+
+void go_to(double x, double y){
+	x = mm2steps(x);
+	y = mm2steps(y);
+	int cable_left = left.get_current_step();
+	int cable_right = right.get_current_step();
+	int steps_left = sqrt(x * x + y * y); //total cable step
+	int steps_right = sqrt((mm2steps(width) - x) * (mm2steps(width) - x) + y * y);
+	steps_left -= cable_left;
+	steps_right -= cable_right;
+	left.step(steps_left);
+	right.step(steps_right);
+}
+
 
 int main() {
 	wiringPiSetupGpio(); // Initalize Pi GPIO
 	printf("Hello world!\n");
 
+	//left.write_mm(963);
+	//right.write_mm(900);
+
 	left.setup();
 	right.setup();
 
-	right.step(10000, 10);
+	std::cout << right.get_current_step() << '\n';
+	//right.step(-4000000, 4);
+	//left.step(-40000000, 4);
+	go_to(400, 600);
+	std::cout << right.get_current_step() << '\n';
+
+	left.save_cable_length();
+	right.save_cable_length();
 }
